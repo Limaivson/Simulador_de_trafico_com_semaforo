29a5afabb8c45938d0c9dca6bb7ccdf7efd4662f
Ivson <ils3@cin.ufpe.br>
2023-04-27 20:18:11 -0300

    Ajustes teste_2

--- src/tests/test_2.py
+++ src/tests/test_2.py
@@ -1,24 +1,53 @@
-from trafficSimulator import *
+from src.trafficSimulator import *
 
 # Create simulation
 sim = Simulation()
 
 # Add multiple roads
 sim.create_roads([
-    ((0, 100), (148, 100)),
-    ((148, 100), (300, 100)),
-    
-    ((150, 0), (150, 98)),
-    ((150, 98), (150, 200)),
+    # Via 40
+    ((300, 30), (350, 30)),
+    # Via 41
+    ((350, 30), (450, 30)),
+
+    # Via 50, indo para a direita
+    ((300, 100), (348, 100)),
+    # Via 51 indo para a direita
+    ((348, 100), (450, 100)),
+    # Via 50, indo para a esquerda
+    ((350, 104), (300, 104)),
+    ((450, 104), (350, 104)),
+    # Via 60
+    ((300, 170), (350, 170)),
+
+    # Via 70 indo para baixo
+    ((351, 0), (351, 99)),
+    # 3 Via inferior
+
+    # 4 Via para a esquerda
+    ((50, 0), (50, 98)),
+
+    ((370, 100), (430, 29)),
 ])
 
+# config = {
+#     "cycle": [(False, True), (True, False)],
+#     "slow_distance": 40,
+#     "slow_factor": 10,
+#     "stop_distance": 15,
+#     "semaphore_1_time": 2, # tempo de mudança de estado para o semáforo 1
+#     "semaphore_2_time": 1 # tempo de mudança de estado para o semáforo 2
+# }
+
+decisions = []
 sim.create_gen({
     'vehicle_rate': 20,
     'vehicles': [
-        [1, {"path": [0, 1]}],
+        [1, {"path": [4, 1]}],
         [1, {"path": [0, 3]}],
         [1, {"path": [2, 3]}],
-        [1, {"path": [2, 3]}]
+        [1, {"path": [2, 3]}],
+
     ]
 })
 
