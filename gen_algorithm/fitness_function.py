c9269ebdb348551fa2c530a6b907c9172f230f9a
Rubem Novellino <rmnf@cin.ufpe.br>
2023-04-27 19:49:45 -0300

    Feature: 2 to 4 way algorithm with library

--- gen_algorithm/fitness_function.py
+++ gen_algorithm/fitness_function.py
@@ -4,7 +4,7 @@ from traffic_sim import Street
 from math import ceil
 from random import randint
 
-def fitness_func(light_control: TrafficLight, ways: int):
+def ff(light_control: TrafficLight, ways: int):
     time_min  = light_control.get_time_min()
     time_max  = light_control.get_time_max()
     coef      = light_control.get_coef()
@@ -17,12 +17,12 @@ def fitness_func(light_control: TrafficLight, ways: int):
     green = randint(1, ways)
     for street in range(ways):
         length = randint(200, 500)
-        print(f"Street {street}: {length}m")
+        # print(f"Street {street}: {length}m")
         if street == green:
             streets.append(Street(iter_time, length, False))
         else:
             streets.append(Street(iter_time, length, True))
-    input()
+    # input()
 
     green_time_iterations = ceil(time_min/iter_time)
     total_wait_time = 0
@@ -36,8 +36,8 @@ def fitness_func(light_control: TrafficLight, ways: int):
             exited_cars = street.iterate(iter_time)
             total_wait_time += exited_cars["stopped time"]
             total_car_count += exited_cars["car count"]
-            print(str(street) + "\t", end="")
-        print()
+            # print(str(street) + "\t", end="")
+        # print()
         if iteration % summon_iter == 0:
             streets[randint(0,len(streets)-1)].summon_car()
         green_time_iterations -= 1
@@ -47,10 +47,10 @@ def fitness_func(light_control: TrafficLight, ways: int):
             street.red_light = True
         total_car_count += street.car_count()
         total_wait_time += street.stopped_time_sum()
-    print(total_wait_time)
-    print(total_car_count)
+    # print(total_wait_time)
+    # print(total_car_count)
 
     return 100 * total_wait_time / (total_car_count * sim_time)
 
 if __name__ == "__main__":
-    print(fitness_func(TrafficLight(50, 10, 0.01), 3))
+    print(ff(TrafficLight(50, 10, 0.01), 3))
